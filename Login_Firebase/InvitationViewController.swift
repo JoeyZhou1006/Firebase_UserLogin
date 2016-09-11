3f251662e79c32015addae7a284c1866429a0016
yu zhou <zy936139466@gmail.com>
2016-09-12 03:57:54 +0930

    new feature validate user added,so now business users can use their invitation code to get validated, a background function will check whether the input code is stored in the firebase database or not

--- Login_Firebase/InvitationViewController.swift
+++ Login_Firebase/InvitationViewController.swift
@@ -7,15 +7,53 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class InvitationViewController: UIViewController {
     
     
     
     //this function here will validate the invitation code that is being input, if succeed, then use segue to go to the sign in page
+    var ref: FIRDatabaseReference!
     
-    @IBAction func ValidateVode(sender: AnyObject) {
+    var refHandle: UInt!
+    
+    var codeRetrived = ""
+    
+    @IBOutlet weak var inputCode: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        
+        //create a reference to the firebase datbase, retrieve the key value set,
+        ref = FIRDatabase.database().reference()
+        refHandle = ref.observeEventType(FIRDataEventType.Value, withBlock: {
+            (snaptshot) in
+            
+            //retrieve all the data sets in the database
+            let dataDict = snaptshot.value as! [String: AnyObject]
+            print(dataDict)
+            self.codeRetrived = dataDict["InvitationCode"] as! String
+            
+            print(self.codeRetrived)
+            
+        })
+        
+        
+        
+    }
+    
+    @IBAction func ValidateCode(sender: AnyObject) {
+        if (inputCode.text != codeRetrived) {
+            print("validataion failed, please contact joeyzhouaus@gmail.com for enquiries")
+        }else{
+            print("congratulations, you can start to create your own business page")
+        
+        }
+ 
+     
         
     }
     
