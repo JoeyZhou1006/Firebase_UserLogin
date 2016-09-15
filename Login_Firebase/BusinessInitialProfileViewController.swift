b613e42e24d4dd602931131a5dd8a28364e57cb8
yu zhou <zy936139466@gmail.com>
2016-09-16 00:10:37 +0930

    current stable version before importing github framework task queue which can help me organize methods execute in a serial way

--- Login_Firebase/BusinessInitialProfileViewController.swift
+++ Login_Firebase/BusinessInitialProfileViewController.swift
@@ -12,7 +12,7 @@ import Firebase
 class BusinessInitialProfileViewController: UIViewController {
     
     //create firebase storage reference
-    var StorageRef = FIRDatabase.database().reference()
+    let StorageRef = FIRDatabase.database().reference()
     
     
     
@@ -24,6 +24,8 @@ class BusinessInitialProfileViewController: UIViewController {
     }
     
     
+    var myGroup = dispatch_group_create()
+    
     @IBOutlet weak var inputName: UITextField!
     
     
@@ -33,24 +35,87 @@ class BusinessInitialProfileViewController: UIViewController {
     @IBAction func submitBusinessName(sender: AnyObject) {
         
         //check whether someone already registered with this names
-       // checkWhetherNameExists()
+        
+        
+        //checkWhetherBusinessNameExists()
         //sent the current users name associated with the uid to the firebase storage
         
         //1. create a reference to the storage of the firebase
 
         //workng code
-        StorageRef.child("businessUsers").child(Uid).child("Business_Names").setValue(inputName.text)
+        //save the business names associated to the business users uid under the business users database
+//        
+        
+        //var exsits = false
+      //  var myGroup = dispatch_group_create()
+        
+//        
+//        dispatch_group_enter(myGroup)
+//        exsits = checkWhetherBusinessNameExists()
+//        print("finished request")
+//        print("and the exists should be true");
+//        dispatch_group_leave(myGroup)
+//    
+//        dispatch_group_notify(myGroup, dispatch_get_main_queue(), {
+        var exsits = false
+        exsits = checkWhetherBusinessNameExists()
+        
+            if(exsits == false){
+                self.StorageRef.child("businessUsers").child(self.Uid).child("Business_Names").setValue(self.inputName.text)
+                print("the name is not exists")
+            }
+            else{
+                print("the name is already existsssss")
+            }
+
+        
+
+        
+        
+   
+        
 
         
     }
     
     
     //this function should search through all the business users, and check the child attribute "name" whether exsits or not
-
-    
     @IBAction func dismissCurrentView(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: {})
     }
+    
+    
+    func checkWhetherBusinessNameExists()-> Bool{
+        var exists = false
+        
+        
+        StorageRef.child("businessUsers").observeSingleEventOfType(FIRDataEventType.Value, withBlock: {(snapshot) in
+           
+            //if let snapshots = snapshot.children.allObjects
+            //go through all the business users profile stored in firebase
+            
+            for child in snapshot.children{
+                
+                
+                //if the business name is already exists, set exists == true, and jump out of the loop,
+                if (child.value["Business_Names"] as! String? == self.inputName.text){
+                   print("this business name is already occupied")
+                    //true value is not inserted properly
+                    exists = true
+                   
+                    return
+                }
+            }
+            
+        })
+        
+        print("should return trueeeeeeeeeeee, while the result is ",exists)
+
+        return exists
+    
+    }
+    
+    
     
     
 
